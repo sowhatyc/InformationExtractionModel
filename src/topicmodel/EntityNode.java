ff4069054b272ff015d2d10b2853505971cb29d5
sowhatyc <75553395@qq.com>
2014-03-15 21:44:53 +0800

    增加了一个新的类EntityNode，用于表示一个Data Record

---
+++ src/topicmodel/EntityNode.java
@@ -0,0 +1,17 @@
+package topicmodel;
+
+import java.util.List;
+
+public class EntityNode {
+	
+	private List<TextNode> entityUnit = null;
+
+	public List<TextNode> getEntityUnit() {
+		return entityUnit;
+	}
+
+	public void setEntityUnit(List<TextNode> entityUnit) {
+		this.entityUnit = entityUnit;
+	}
+
+}
